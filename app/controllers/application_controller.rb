8fbc26ddc084068e4402be9c0e7c3e4e56184e88
Suraj Bhattarai <surajbh@hotmail.com>
2013-02-18 19:33:50 -0500

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_admin_user
-    return nil if user_signed_in? && !current_user.admin?
+    return nil if user_signed_in? && !current_user.is_admin?
     current_user
   end
   
