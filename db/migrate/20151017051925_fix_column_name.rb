5588ff80a2146c2617d8bbe264ed3086e311c63d
manish bajracharya <mbajra1@students.towson.edu>
2015-11-22 01:36:37 -0500

    change in migration column name mailing addressses table

--- db/migrate/20151017051925_fix_column_name.rb
+++ db/migrate/20151017051925_fix_column_name.rb
@@ -4,7 +4,7 @@ class FixColumnName < ActiveRecord::Migration
     rename_column :cs_applications, :middle, :middle_name
     rename_column :cs_applications, :last, :last_name
     rename_column :cs_applications, :tuid, :towson_id_number
-    rename_column :cs_applications, :line1, :address_line1
-    rename_column :cs_applications, :line2, :address_line2
+    rename_column :mailing_addresses, :line1, :address_line1
+    rename_column :mailing_addresses, :line2, :address_line2
   end
 end
