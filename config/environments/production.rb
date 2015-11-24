3a19a618ce153f4e93b5efc89e3997066af407f0
manish bajracharya <mbajra1@students.towson.edu>
2015-11-24 01:12:00 -0500

    smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
+      :address              => "webmail.towson.edu",
       :port                 => 587,
-      :user_name            => ENV['gmail_username'],
-      :password             => ENV['gmail_password'],
+      :user_name            => "mbajra1@students.towson.edu",
+      :password             => ENV['SMTP_PASSWORD'],
       :authentication       => "plain",
-      :enable_starttls_auto => true
+      :enable_starttls_auto => false
   }
-
+  config.action_mailer.raise_delivery_errors = true
 end
