7e4a9943fb608ea631c626e788b86c4eadf0ed54
manish bajracharya <mbajra1@students.towson.edu>
2015-11-30 20:32:45 -0500

    SMTP configuration in Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.towson.edu",
+      :address              => "smtp.gmail.com",
       :port                 => 587,
       :user_name            => "mbajra1@students.towson.edu",
       :password             => ENV['SMTP_PASSWORD'],
